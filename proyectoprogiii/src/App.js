ae6b95b7d814ef2e4f510e6bf4bd8500d58bb568
esperanza uranga <euranga@udesa.edu.ar>
2021-04-21 12:21:43 -0300

    header y footer

--- proyectoprogiii/src/App.js
+++ proyectoprogiii/src/App.js
@@ -5,20 +5,32 @@ import "./app.css"
 import Addcard from './components/Addcard'
 import Filtros from './components/Filtros';
 import NewCardForm from "./components/NewCardForm"
+
 function App() {
   return (
-    <div className = "App">
-      <Filtros/>
-      < Item />
-      < Item />
-      < Item />
-      < Item />
-      < Item />
-      < Item />
-      < Item />
-      <Addcard/>
-      <NewCardForm/>
-    </div>
+    <React.Fragment>
+      <header className = 'header'>
+        <h1>HEADER</h1>
+      </header>
+
+      <div className = "App">
+        <Filtros/>
+        < Item />
+        < Item />
+        < Item />
+        < Item />
+        < Item />
+        < Item />
+        < Item />
+        <Addcard/>
+        <NewCardForm/>
+      </div>
+
+      <footer>
+        <p>Desarrolladores: Esperanza Uranga, Francesca Porro Toyos y Segundo Ustariz</p>
+      </footer>
+
+    </React.Fragment>
   );
 }
 
--- proyectoprogiii/src/components/Filtros.js
+++ proyectoprogiii/src/components/Filtros.js
@@ -1,5 +1,6 @@
 import React from 'react'
 function Filtros (){
+
 return (
     <div className='filterContainer'>
         <p>Filtros</p>
