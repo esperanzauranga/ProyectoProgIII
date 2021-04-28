9631b71d01e835cc05cfa5ccf15ede22a87e3fc1
Segundo <sustariz@udesa.edu.ar>
2021-04-28 16:25:43 -0300

    Filtros (faltan cosas)

--- proyectoprogiii/src/App.js
+++ proyectoprogiii/src/App.js
@@ -10,11 +10,13 @@ class App extends React.Component {
   constructor (){
     super() 
     this.state= {
-      showaddcard:false, items:[]
+      showaddcard:false, items:[], filterItems:[], hasFilters:false
     }
     this.showNewUserForm = this.showNewUserForm.bind(this);
     this.addNewUser = this.addNewUser.bind(this);
     this.fetchCall = this.fetchCall.bind(this)
+    this.filter = this.filter.bind(this)
+    this.clearFilter = this.clearFilter.bind(this)
   }
   
 
@@ -22,7 +24,7 @@ class App extends React.Component {
     this.fetchCall('https://randomuser.me/api/?results=15')
 }
 componentDidUpdate(){
-   console.log (this.state.items)
+
 }
 fetchCall(url){
     fetch(url)
@@ -86,10 +88,48 @@ fetchCall(url){
   
  
     
-  
+  filter(filters) {
+    let tempValues=[]
+   filters.map(filterIt=>{
+
+    if(filterIt.nombre){
+
+     this.state.filterItems.push(  this.state.items.filter(item =>{
+       
+        if(item.name.first.toUpperCase()===filterIt.nombre.toUpperCase()) {
+          
+          return item
+        }
+      }))
+     
+    }
+
+   })
+    this.setState({
+      ...this.state,
+      filterItems:this.state.filterItems,
+      hasFilters: true
+    })
+   
+    /*
+    if(filters.apellido !=""){
+      this.state.filterItems.push( this.state.items.filter(item =>{
+         if(item.name.last == filters.apellido) return item
+       }))
+     }
+    */
+  }
+  clearFilter() {
+    this.setState({
+      ...this.state,
+      hasFilters:false,
+      filterItems:[]
+    })
+    
+  }
 
  render (){ 
-   const {showaddcard,items}=this.state
+   const {showaddcard,items,hasFilters,filterItems}=this.state
    return (
     <React.Fragment>
       <header className = 'header'>
@@ -97,8 +137,8 @@ fetchCall(url){
       </header>
 
       <div className = "App">
-        <Filtros/>
-        < ItemsConteiner items={items} />
+        <Filtros filter={this.filter} clearFilter={this.clearFilter}/>
+       {hasFilters ? < ItemsConteiner items={filterItems} /> : < ItemsConteiner items={items} />}
        {showaddcard ? <NewCardForm addNewUser={this.addNewUser}/> : null}
         <Addcard showNewUserForm={this.showNewUserForm}/>
     
--- proyectoprogiii/src/components/ItemsConteiner.js
+++ proyectoprogiii/src/components/ItemsConteiner.js
@@ -1,34 +1,55 @@
 import React, {Component} from 'react'
 import Item from './Item'
-function ItemsConteiner ({items}) {
-     
-        return (
-    <div class="uk-slider-container-offset" uk-slider>
-
-    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
-
-        <ul class="uk-slider-items uk-child-width-1-2@s uk-grid">
-        {
-            items.map((item,idx)=>{
-                return <Item
-                        key={idx}
-                        item={item}
-                />
-            })
+class ItemsConteiner extends Component {
+     constructor(props){
+         super(props)
+         console.log (props)
+         this.state={
+             items:[]
+         }
         }
-           
-        </ul>
+        componentDidUpdate(){
+            console.log(this.props.items)
+            
+        
+        }
 
-        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
-        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
-
-    </div>
-
-    <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
-
-</div>
-
-)
+        componentDidMount() {
+            console.log(this.props)
+            
+        }
+       
+        render(){
+            const {items} = this.props
+            return (
+                <div class="uk-slider-container-offset" uk-slider>
+            
+                <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
+            
+                    <ul class="uk-slider-items uk-child-width-1-2@s uk-grid">
+                    {
+                        items.map((item,idx)=>{
+                            return <Item
+                                    key={idx}
+                                    item={item}
+                            />
+                        })
+                    }
+                       
+                    </ul>
+            
+                    <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+                    <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+            
+                </div>
+            
+                <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
+            
+            </div>
+            
+            )
+        }
+       
     }
 
 
