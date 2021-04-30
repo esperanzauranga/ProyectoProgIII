ac7bf77c62a86f9376f2eb4802e8ba0301d991b8
esperanza uranga <euranga@udesa.edu.ar>
2021-04-29 23:38:44 -0300

    css

--- proyectoprogiii/src/App.js
+++ proyectoprogiii/src/App.js
@@ -24,196 +24,196 @@ class App extends React.Component {
 
   componentDidMount(){
     this.fetchCall('https://randomuser.me/api/?results=15')
-}
-componentDidUpdate(){
-
-}
-fetchCall(url){
-    fetch(url)
-    .then (resultado=>{
-        return resultado.json()
-    
-    })
-    .then(usuarios=>{
-        this.configUserFetch(usuarios.results)
-        this.setState({
-          ...this.state, 
-          items:usuarios.results})
-    })
-    .catch (error=>{console.log(error)})
-    
-}
-// para que muestre mejor l fecha
-configUserFetch (usuarios) {
-usuarios.map ((usuario,idx) => {
-  usuario.id = idx
-  usuario.registered.date= usuario.registered.date.split('T')[0]
-})
-
-}
-
-  addNewUser (newUser){
-    let userConfig= {
-     name:{
-       first:newUser.nombre,
-       last:newUser.apellido
-     } ,
-     email: newUser.email,
-     registered: {
-       date: newUser.fechadenacimiento
-     },
-     phone: newUser.telefono,
-     picture: {
-      large: '/perfil-01.jpg'
-     },
-     location: {
-       street: {
-         name: '',
-         number: 123
-       },
-       city: newUser.ciudad,
-       country: newUser.pais,
-       state: '',
-       postcode: newUser.cp
-     }
-    }
-    let temp = this.state.items
-    temp.push(userConfig)
-    this.setState({
-      ...this.state, 
-      items:temp
-    })
-    this.setState({
-      ...this.state,
-      showaddcard:false
-    })
   }
-  showNewUserForm (){
-    console.log('funciona')
-    this.setState({
-      ...this.state,
-      showaddcard:true
-    })
+  componentDidUpdate(){
+
   }
-  
- 
-    
-  filter(filters) {
-    let tempValues=[]
-   filters.map(filterIt=>{
-
-    if(filterIt.nombre){
-
-     tempValues.push(  this.state.items.filter(item =>{
-       
-        if(item.name.first.toUpperCase().includes(filterIt.nombre.toUpperCase())) {
-          
-          return item
-        }
-      }))
-     
-    }
-    if(filterIt.apellido){
-
-      tempValues.push(  this.state.items.filter(item =>{
-        
-         if(item.name.last.toUpperCase()===filterIt.apellido.toUpperCase()) {
-           
-           return item
-         }
-       }))
+  fetchCall(url){
+      fetch(url)
+      .then (resultado=>{
+          return resultado.json()
       
-     }
-     if(filterIt.edad){
-
-      tempValues.push(  this.state.items.filter(item =>{
-        
-         if(item.dob.age.toString()===filterIt.edad.toString()) {
-           
-           return item
-         }
-       }))
-      
-     }
-  
-   })
-    this.setState({
-      ...this.state,
-      filterItems:tempValues[0],
-      hasFilters: true
-    })
-   
-    /*
-    if(filters.apellido !=""){
-      this.state.filterItems.push( this.state.items.filter(item =>{
-         if(item.name.last == filters.apellido) return item
-       }))
-     }
-    */
-  }
-  clearFilter() {
-    this.setState({
-      ...this.state,
-      hasFilters:false,
-      filterItems:[]
-    })
-    
-  }
-  deleteItem (itemId) {
-  // let tempItems= this.state.items.filter(item=>{
-  //  if (item.id!==itemId) return item
-  //})
-    this.setState({
-      ...this.state,
-      items: this.state.items.filter(item=>{
-        if (item.id!==itemId) return item
-      }),
-      filterItems: this.state.filterItems.filter(item=>{
-        if (item.id!==itemId) return item
       })
-    })
+      .then(usuarios=>{
+          this.configUserFetch(usuarios.results)
+          this.setState({
+            ...this.state, 
+            items:usuarios.results})
+      })
+      .catch (error=>{console.log(error)})
+      
   }
-  addUsersToList(amount=5){
-    fetch("https://randomuser.me/api/?results="+amount)
-    .then (resultado=>{
-        return resultado.json()
+  // para que muestre mejor l fecha
+  configUserFetch (usuarios) {
+  usuarios.map ((usuario,idx) => {
+    usuario.id = idx
+    usuario.registered.date= usuario.registered.date.split('T')[0]
+  })
+
+  }
+
+    addNewUser (newUser){
+      let userConfig= {
+      name:{
+        first:newUser.nombre,
+        last:newUser.apellido
+      } ,
+      email: newUser.email,
+      registered: {
+        date: newUser.fechadenacimiento
+      },
+      phone: newUser.telefono,
+      picture: {
+        large: '/perfil-01.jpg'
+      },
+      location: {
+        street: {
+          name: '',
+          number: 123
+        },
+        city: newUser.ciudad,
+        country: newUser.pais,
+        state: '',
+        postcode: newUser.cp
+      }
+      }
+      let temp = this.state.items
+      temp.push(userConfig)
+      this.setState({
+        ...this.state, 
+        items:temp
+      })
+      this.setState({
+        ...this.state,
+        showaddcard:false
+      })
+    }
+    showNewUserForm (){
+      console.log('funciona')
+      this.setState({
+        ...this.state,
+        showaddcard:true
+      })
+    }
+    
+  
+      
+    filter(filters) {
+      let tempValues=[]
+    filters.map(filterIt=>{
+
+      if(filterIt.nombre){
+
+      tempValues.push(  this.state.items.filter(item =>{
+        
+          if(item.name.first.toUpperCase().includes(filterIt.nombre.toUpperCase())) {
+            
+            return item
+          }
+        }))
+      
+      }
+      if(filterIt.apellido){
+
+        tempValues.push(  this.state.items.filter(item =>{
+          
+          if(item.name.last.toUpperCase()===filterIt.apellido.toUpperCase()) {
+            
+            return item
+          }
+        }))
+        
+      }
+      if(filterIt.edad){
+
+        tempValues.push(  this.state.items.filter(item =>{
+          
+          if(item.dob.age.toString()===filterIt.edad.toString()) {
+            
+            return item
+          }
+        }))
+        
+      }
     
     })
-    .then(usuarios=>{
-        this.configUserFetch(usuarios.results)
-        let tempUsuarios=usuarios.results.map(usuario=>{
-          this.state.items.push(usuario)
-        })
-        console.log (tempUsuarios)
-        console.log (amount)
-        this.setState({
-          ...this.state, 
-          items:this.state.items
-        })
-    })
-    .catch (error=>{console.log(error)})
-  }
- render (){ 
-   const {showaddcard,items,hasFilters,filterItems}=this.state
-   return (
-    <React.Fragment>
-      <header className = 'header'>
-        <h1>HEADER</h1>
-      </header>
-
-      <div className = "App">
-        <Filtros filter={this.filter} clearFilter={this.clearFilter}/>
-       {hasFilters ? < ItemsConteiner items={filterItems}  deleteItem={this.deleteItem} /> : < ItemsConteiner items={items} deleteItem={this.deleteItem} />}
-       {showaddcard ? <NewCardForm addNewUser={this.addNewUser}/> : null}
-        <Addcard showNewUserForm={this.showNewUserForm} addUsersToList={this.addUsersToList}/>
+      this.setState({
+        ...this.state,
+        filterItems:tempValues[0],
+        hasFilters: true
+      })
     
-      </div>
+      /*
+      if(filters.apellido !=""){
+        this.state.filterItems.push( this.state.items.filter(item =>{
+          if(item.name.last == filters.apellido) return item
+        }))
+      }
+      */
+    }
+    clearFilter() {
+      this.setState({
+        ...this.state,
+        hasFilters:false,
+        filterItems:[]
+      })
+      
+    }
+    deleteItem (itemId) {
+    // let tempItems= this.state.items.filter(item=>{
+    //  if (item.id!==itemId) return item
+    //})
+      this.setState({
+        ...this.state,
+        items: this.state.items.filter(item=>{
+          if (item.id!==itemId) return item
+        }),
+        filterItems: this.state.filterItems.filter(item=>{
+          if (item.id!==itemId) return item
+        })
+      })
+    }
+    addUsersToList(amount=5){
+      fetch("https://randomuser.me/api/?results="+amount)
+      .then (resultado=>{
+          return resultado.json()
+      
+      })
+      .then(usuarios=>{
+          this.configUserFetch(usuarios.results)
+          let tempUsuarios=usuarios.results.map(usuario=>{
+            this.state.items.push(usuario)
+          })
+          console.log (tempUsuarios)
+          console.log (amount)
+          this.setState({
+            ...this.state, 
+            items:this.state.items
+          })
+      })
+      .catch (error=>{console.log(error)})
+    }
+  render (){ 
+    const {showaddcard,items,hasFilters,filterItems}=this.state
+    return (
+      <React.Fragment>
+        <header className = 'header'>
+          <h1>HEADER</h1>
+        </header>
 
-      <footer>
-        <p>Desarrolladores: Esperanza Uranga, Francesca Porro Toyos y Segundo Ustariz</p>
-      </footer>
+        <div className = "App">
+          <Filtros filter={this.filter} clearFilter={this.clearFilter}/>
+        {hasFilters ? < ItemsConteiner items={filterItems}  deleteItem={this.deleteItem} /> : < ItemsConteiner items={items} deleteItem={this.deleteItem} />}
+        {showaddcard ? <NewCardForm addNewUser={this.addNewUser}/> : null}
+          <Addcard showNewUserForm={this.showNewUserForm} addUsersToList={this.addUsersToList}/>
+      
+        </div>
 
-    </React.Fragment>
+        <footer>
+          <p>Desarrolladores: Esperanza Uranga, Francesca Porro Toyos y Segundo Ustariz</p>
+        </footer>
+
+      </React.Fragment>
   );
  }
 }
