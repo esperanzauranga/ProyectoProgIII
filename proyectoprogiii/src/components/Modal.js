2e6f7bbd09c70dd0c5ea95a1d090d7533d546552
francesca <franchuporro@hotmail.com>
2021-04-30 00:24:35 -0300

    aparece el ver detalle bien

--- proyectoprogiii/src/components/Modal.js
+++ proyectoprogiii/src/components/Modal.js
@@ -8,15 +8,15 @@ function Modal ({item, cerrarDetalle}) {
            
                 <div className='modal-background'>
          
-                    <div className='modal'>
+                    <div className='modal' class="uk-card-body" >
                         <button onClick={cerrarDetalle} >X</button>
-                        <p>calle y numero: {item.location.street.name}  {item.location.street.number}</p>
-                        <p>ciudad/estado: {item.location.city}, {item.location.state}</p>
-                        <p>pais: {item.location.country}</p>
-                        <p>codigo postal: {item.location.postcode}</p>
-                        <p>fecha de registro: {item.registered.date}</p>
-                        <p>telefono: {item.phone}</p>
-                         <p>edad: {item.dob.age}</p>
+                        <p>Calle y numero: {item.location.street.name}  {item.location.street.number}</p>
+                        <p>Ciudad/Estado: {item.location.city}, {item.location.state}</p>
+                        <p>Pais: {item.location.country}</p>
+                        <p>Codigo postal: {item.location.postcode}</p>
+                        <p>Fecha de registro: {item.registered.date}</p>
+                        <p>Telefono: {item.phone}</p>
+                         <p>Edad: {item.dob.age}</p>
 
                     </div>
                     
